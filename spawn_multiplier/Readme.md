ea7375d13b259914f96c5e9973d48cfc4b9cd877
apple1417 <git@apple1417.dev>
2025-05-04 19:15:05 +1200

    port spawn multipier
    fixes #10

---
+++ spawn_multiplier/Readme.md
@@ -0,0 +1,50 @@
+# Spawn Multiplier
+## Softlocks
+This mod is known to cause softlocks. The game handles "kill all enemies" objectives in half a dozen
+different ways for example, and not all of them interact with this properly. When something
+softlocks, SQing and disabling the mod, completing the objective without it, and then re-enabling
+it, should let you progress further.
+
+These softlocks are not considered bugs, they're just the cost of doing business. The only bugs are
+ones that break your save, and prevent progressing even after disabling the mod. This is not to say
+that spawns which softlock won't be blacklisted at some point, just that I won't spend time actively
+hunting them all down.
+
+The current list of blacklisted enemies is:
+- Thousand Cuts Brick - You can't hand in the note if there are extras.
+- First Bunker autocannon - Extras will be invicible, but have to be killed to progress the quest.
+- Claptrap worshippers - Extras will be friendly, but must be killed to progress the quest.
+- Story kill Uranus - Extras don't spawn, but must be killed to open the doorway back.
+- The first four resurrected skeletons in each grave during My Dead Brother - Extras don't spawn,
+  but must be killed to progress.
+
+## Changelog
+
+### Spawn Multiplier v2
+- Upgraded to v3 sdk.
+
+### Spawn Multiplier v1.5
+- Updated to use some of the new features from SDK version 0.7.8. Most notably, the enabled state is
+  now saved over game launches.
+
+### Spawn Multiplier v1.4
+- Fixed a few places where enemy spawns had an additional cap, which will now be increased along
+  with the others.
+- Blacklisted a few enemies that would cause softlocks if multiplied.
+- Fixed potential compatibility issue with an upcoming version of the sdk.
+
+### Spawn Multiplier v1.3
+- Prevented multiplying spawn counts for some non-enemy actors, which could cause softlocks in some
+  cases (e.g. Burrows' generators).
+
+### Spawn Multiplier v1.2
+- Fixed that spawn limit changes would not be reapplied on map change, they'd always revert to
+  standard.
+- Fixed some more instances where spawn counts would round down to 0.
+
+### Spawn Multiplier v1.1
+- Fixed that changing the multiplier could occasionally round spawn counts down to 0.
+- Fixed that disabling the mod would not revert spawn counts.
+
+### Spawn Multiplier v1.0
+- Initial Release.
---
+++ spawn_multiplier/__init__.py
@@ -0,0 +1,236 @@
+from enum import StrEnum
+from typing import Any
+
+import unrealsdk
+from mods_base import SliderOption, SpinnerOption, build_mod, hook
+from unrealsdk.hooks import Type
+from unrealsdk.unreal import BoundFunction, UObject, WeakPointer, WrappedStruct
+
+
+class SpawnLimitType(StrEnum):
+    Standard = "Standard"
+    Linear = "Linear"
+    Unlimited = "Unlimited"
+
+
+last_pop_master: WeakPointer = WeakPointer()
+last_pop_master_original_limit: int | None = None
+
+
+def update_spawn_limit(pop_master: UObject, limit_type: SpawnLimitType | str) -> None:
+    """
+    Updates the spawn limit, taking into account the new scaling type.
+
+    Args:
+        pop_master: The population master holding the spawn limit to update.
+        limit_type: What scaling type the limit should use.
+    """
+    global last_pop_master, last_pop_master_original_limit
+
+    if (
+        existing_pop_master := last_pop_master()
+    ) is not None and last_pop_master_original_limit is not None:
+        existing_pop_master.MaxActorCost = last_pop_master_original_limit
+
+    last_pop_master = WeakPointer(pop_master)
+    last_pop_master_original_limit = pop_master.MaxActorCost
+
+    match limit_type:
+        case SpawnLimitType.Linear:
+            pop_master.MaxActorCost *= multiplier_slider.value
+        case SpawnLimitType.Unlimited:
+            pop_master.MaxActorCost = 0x7FFFFFFF
+        case _:
+            pass
+
+
+@SpinnerOption(
+    identifier="Spawn Limit",
+    value=SpawnLimitType.Linear,
+    choices=list(SpawnLimitType),
+    description=(
+        "How to handle the spawn limit."
+        f" {SpawnLimitType.Standard}: Don't change it;"
+        f" {SpawnLimitType.Linear}: Increase linearly with the multiplier;"
+        f" {SpawnLimitType.Unlimited}: Remove it."
+    ),
+)
+def spawn_limit_spinner(opt: SpinnerOption, new_value: str) -> None:  # noqa: D103
+    if not opt.mod or not opt.mod.is_enabled:
+        return
+    update_spawn_limit(
+        (
+            unrealsdk.find_class("GearboxGlobals")
+            .ClassDefaultObject.GetGearboxGlobals()
+            .GetPopulationMaster()
+        ),
+        new_value,
+    )
+
+
+@hook("GearboxFramework.PopulationMaster:SpawnPopulationControlledActor")
+def spawn_pop_controlled_actor(  # noqa: D103
+    obj: UObject,
+    _args: WrappedStruct,
+    _ret: Any,
+    _func: BoundFunction,
+) -> None:
+    if obj == last_pop_master():
+        return
+    update_spawn_limit(obj, spawn_limit_spinner.value)
+
+
+DEN_BLACKLIST: set[str] = {
+    # Thousand Cuts Brick
+    "Grass_Cliffs_Combat.TheWorld:PersistentLevel.PopulationOpportunityDen_16",
+    # First Bunker autocannon
+    "Boss_Cliffs_CombatLoader.TheWorld:PersistentLevel.PopulationOpportunityDen_4",
+    # Claptrap worshippers
+    "Sage_RockForest_Dynamic.TheWorld:PersistentLevel.PopulationOpportunityDen_11",
+    # Story kill Uranus
+    "Helios_Mission_Main.TheWorld:PersistentLevel.PopulationOpportunityDen_6",
+    # Resurrected skeletons in My Dead Brother - first grave
+    "Dungeon_Mission.TheWorld:PersistentLevel.PopulationOpportunityDen_4",
+    # Resurrected skeletons in My Dead Brother - second grave
+    "Dungeon_Mission.TheWorld:PersistentLevel.PopulationOpportunityDen_12",
+    # Resurrected skeletons in My Dead Brother - third grave
+    "Dungeon_Mission.TheWorld:PersistentLevel.PopulationOpportunityDen_9",
+}
+ENCOUNTER_BLACKLIST: set[str] = set()
+
+
+def can_den_be_multiplied(den: UObject | None) -> bool:
+    """
+    Checks if a population den is allowed to be multiplied.
+
+    Args:
+        den: The den to check.
+    Returns:
+        True if the den is allowed to be multiplied.
+    """
+    if den is None or den._path_name() in DEN_BLACKLIST or (pop_def := den.PopulationDef) is None:
+        return False
+
+    return all(
+        (factory := actor.SpawnFactory) is not None
+        and factory.Class.Name
+        not in (
+            "PopulationFactoryBlackMarket",
+            "PopulationFactoryInteractiveObject",
+            "PopulationFactoryVendingMachine",
+        )
+        for actor in pop_def.ActorArchetypeList
+    )
+
+
+def multiply_den_if_allowed(den: UObject | None, adjustment: float) -> None:
+    """
+    Multiplies spawns of a population den, if allowed.
+
+    Args:
+        den: The den to multiply.
+        adjustment: How much to multiply spawns by.
+    """
+    if den is None or not can_den_be_multiplied(den):
+        return
+    den.SpawnData.MaxActiveActors = round(den.SpawnData.MaxActiveActors * adjustment)
+    den.MaxActiveActorsIsNormal = round(den.MaxActiveActorsIsNormal * adjustment)
+    den.MaxActiveActorsThreatened = round(den.MaxActiveActorsThreatened * adjustment)
+    den.MaxTotalActors = round(den.MaxTotalActors * adjustment)
+
+
+def multiply_pop_encounter_if_allowed(encounter: UObject | None, adjustment: float) -> None:
+    """
+    Multiplies spawns of a population encounter, if allowed.
+
+    Args:
+        encounter: The encounter to multiply.
+        adjustment: How much to multiply spawns by.
+    """
+    if encounter is None or encounter.PathName(encounter) in ENCOUNTER_BLACKLIST:
+        return
+
+    for wave in encounter.Waves:
+        if (wave_spawn_limits := wave.SpawnLimits) is None:
+            continue
+        if not all(can_den_be_multiplied(den) for den in wave.MemberOpportunities):
+            continue
+
+        for limit in wave_spawn_limits:
+            limit.MaxTotalToSpawn.BaseValueScaleConstant = round(
+                limit.MaxTotalToSpawn.BaseValueScaleConstant * adjustment,
+            )
+            limit.MaxActiveAtATime.BaseValueScaleConstant = round(
+                limit.MaxActiveAtATime.BaseValueScaleConstant * adjustment,
+            )
+
+
+@hook("GearboxFramework.PopulationEncounter:UpdateOpportunityEnabledStates")
+def update_pop_opportunity_enabled_states(  # noqa: D103
+    obj: UObject,
+    args: WrappedStruct,
+    _ret: Any,
+    _func: BoundFunction,
+) -> None:
+    # Seems to be -1 on map load
+    # I've never seen it get called at another time, but seems reasonable to restrict to this
+    if args.nWave != -1:
+        return
+    multiply_pop_encounter_if_allowed(obj, multiplier_slider.value)
+
+
+@hook("WillowGame.PopulationOpportunityDen:PostBeginPlay", Type.POST)
+def den_post_begin_play(  # noqa: D103
+    obj: UObject,
+    _args: WrappedStruct,
+    _ret: Any,
+    _func: BoundFunction,
+) -> None:
+    multiply_den_if_allowed(obj, multiplier_slider.value)
+
+
+def multiply_existing(adjustment: float) -> None:
+    """
+    Adjusts the spawn limit on all already existing objects.
+
+    Args:
+        adjustment: The adjustment to apply.
+    """
+    for den in unrealsdk.find_all("PopulationOpportunityDen"):
+        multiply_den_if_allowed(den, adjustment)
+    for encounter in unrealsdk.find_all("PopulationEncounter"):
+        multiply_pop_encounter_if_allowed(encounter, adjustment)
+
+
+@SliderOption(
+    identifier="Multiplier",
+    value=4,
+    min_value=1,
+    max_value=25,
+    description="The amount to multiply spawns by.",
+)
+def multiplier_slider(opt: SliderOption, new_value: float) -> None:  # noqa: D103
+    if not opt.mod or not opt.mod.is_enabled:
+        return
+    multiply_existing(new_value / multiplier_slider.value)
+
+
+def on_enable() -> None:  # noqa: D103
+    multiply_existing(multiplier_slider.value / 1)
+    update_spawn_limit(
+        (
+            unrealsdk.find_class("GearboxGlobals")
+            .ClassDefaultObject.GetGearboxGlobals()
+            .GetPopulationMaster()
+        ),
+        spawn_limit_spinner.value,
+    )
+
+
+def on_disable() -> None:  # noqa: D103
+    multiply_existing(1 / multiplier_slider.value)
+    if (pop_master := last_pop_master()) is not None and last_pop_master_original_limit is not None:
+        pop_master.MaxActorCost = last_pop_master_original_limit
+
+
+mod = build_mod(options=[multiplier_slider, spawn_limit_spinner])
